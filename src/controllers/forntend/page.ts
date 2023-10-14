5e39271a48d28b142179dd7397d588b949efd200
codingguides <info.codingguides@gmail.com>
2023-10-14 19:50:27 +0530

    update sidebar menu logic

--- src/controllers/forntend/page.ts
+++ src/controllers/forntend/page.ts
@@ -30,62 +30,7 @@ FrontendController.get('/get-menu', async (request: Request, response: Response,
   }
 });
 
-FrontendController.get('/get-side-menu-test', async (request: Request, response: Response, next: NextFunction) => {
-  try {
-
-    await TopicModel.find().then(async(val) => {
-      if (val) {
-        const buildCategoryTree = (categories) => {
-          console.log("1")
-          const categoryMap = {}; // Use a map for faster lookups
-        
-          // Create a map of categories using _id as keys
-          categories.forEach(category => {
-            categoryMap[category._id] = category;
-            category.children = [];
-          });
-        
-          const tree = [];
-        
-          // Organize categories into a tree structure
-          categories.forEach(category => {
-            if (category.parent_id) {
-              const parentCategory = categoryMap[category.parent_id];
-              if (parentCategory) {
-                parentCategory.children.push(category);
-              }
-            } else {
-              tree.push(category);
-            }
-          });
-          console.log("2>>>>",tree)
-        
-          return tree;
-        }
-
-        let list = await buildCategoryTree(val);
-        if(list){
-          response.status(200).send({
-            "status": "SUCCESS",
-            "msg": "Topics details successfully",
-            "payload": list
-          });
-        }
-      } else {
-        response.status(404).send({
-          "status": "ERROR",
-          "msg": "Oops! topic not found."
-        });
-      }
-
-    })
-
-  } catch (error) {
-    next(error)
-  }
-});
-
-FrontendController.get('/get-side-menu', async (request: Request, response: Response, next: NextFunction) => {
+FrontendController.get('/get-sidebar-menu', async (request: Request, response: Response, next: NextFunction) => {
   try {
 
     
@@ -115,70 +60,31 @@ FrontendController.get('/get-side-menu', async (request: Request, response: Resp
       return tree;
     }
     
-    let cateList = [
-      {
-        _id: '650fd9f6abd17375cc8b5eab',
-        name: 'JavaScript',
-        slug: 'javascript'
-      },
-      {
-        _id: '650fdedbabd17375cc8b5ecf',
-        name: 'Quiz for Beginners',
-        slug: 'quiz-for-beginners',
-        parent_id: '650fd9f6abd17375cc8b5eab'
-      },
-      {
-        _id: '650fdfbbabd17375cc8b5ef3',
-        name: 'JS Beginners Quiz 1',
-        slug: 'js-beginners-quiz-1',
-        parent_id: '650fdedbabd17375cc8b5ecf'
-      },
-      {
-        _id: '650fdff9abd17375cc8b5f1a',
-        name: 'JS Beginners Quiz 2',
-        slug: 'js-beginners-quiz-2',
-        parent_id: '650fdedbabd17375cc8b5ecf'
-      },
-      {
-        _id: '65225e6fe1ab957e28c8e080',
-        name: 'Quiz for Intermediate',
-        slug: 'quiz-for-intermediate',
-        parent_id: '650fd9f6abd17375cc8b5eab'
-      },
-      {
-        _id: '6522b557e1ab957e28c8e18d',
-        name: 'Quiz for Advanced',
-        slug: 'quiz-for-advanced',
-        parent_id: '650fd9f6abd17375cc8b5eab'
-      },
-      {
-        _id: '6522b71de1ab957e28c8e1d1',
-        name: 'JS Intermediate Quiz 1',
-        slug: 'js-intermediate-quiz-1',
-        parent_id: '65225e6fe1ab957e28c8e080'
-      },
-      {
-        _id: '6522b772e1ab957e28c8e1ed',
-        name: 'JS Intermediate Quiz 2',
-        slug: 'js-intermediate-quiz-2',
-        parent_id: '65225e6fe1ab957e28c8e080'
-      },
-      {
-        _id: '6522b80de1ab957e28c8e20d',
-        name: 'JS Advanced Quiz 1',
-        slug: 'js-advanced-quiz-1',
-        parent_id: '6522b557e1ab957e28c8e18d'
-      },
-      {
-        _id: '6522b88fe1ab957e28c8e22f',
-        name: 'JS Advanced Quiz 2',
-        slug: 'js-advanced-quiz-2',
-        parent_id: '6522b557e1ab957e28c8e18d'
-      }
-    ];
+
+    let cateList = []
+    let obj = {}
+    await TopicModel.find().then((topicList)=>{
+      topicList.map((topic)=>{
+
+        if(topic.parent_id){
+          obj = {
+            _id: String(topic._id),
+            name: String(topic.name),
+            slug: String(topic.slug),
+            parent_id: String(topic.parent_id)
+          }
+        }else{
+          obj = {
+            _id: String(topic._id),
+            name: String(topic.name),
+            slug: String(topic.slug)
+          }
+        }
+        cateList.push(obj)
+      })
+    })
     
     const categoryTree = await buildCategoryTree(cateList);
-      // console.log(JSON.stringify(categoryTree,null,2));
       if(categoryTree){ 
         response.status(200).send({
           "status": "SUCCESS",
