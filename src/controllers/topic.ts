a3f7f9713980844f463e94b58f4185d4f0d8ce4f
codingguides <info.codingguides@gmail.com>
2023-11-28 23:25:17 +0530

    update

--- src/controllers/topic.ts
+++ src/controllers/topic.ts
@@ -357,6 +357,7 @@ TopicController.get('/list', async (request: Request, response: Response, next: 
 
 // After quiz complect this api call if user logedIN
 TopicController.post('/analytics',
+  check('topic_url').not().isEmpty().withMessage('topic_url is required'),
   check('topic_id').not().isEmpty().withMessage('topic_id is required'),
   check('user_id').not().isEmpty().withMessage('user_id is required'),
   check('attendedQuestionCount').not().isEmpty().withMessage('attendedQuestionCount is required'),
