0dc3168034ad138b574f6d4937711c0f5cbcb2d6
codingguides <info.codingguides@gmail.com>
2023-12-13 20:50:43 +0530

    Update XLSX import api

--- src/controllers/forntend/xlsx.ts
+++ src/controllers/forntend/xlsx.ts
@@ -1,9 +1,8 @@
 import { Router, Request, Response, NextFunction } from "express";
-import { QuestionModel, RelationModel, NewsletterModel, TopicModel, BlogModel, TagsModel, BlogCategoryModel } from "../../models";
-import { body, validationResult } from "express-validator";
-import { isEmpty } from "lodash";
+import { QuestionModel } from "../../models";
 import multer from "multer";
-const readXlsxFile = require('read-excel-file/node');
+var XLSX = require("xlsx");
+
 
 const fs = require('fs');
 const path = require('path');
@@ -25,21 +24,47 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 XlsxController.post('/upload', upload.single("uploadfile"), async (request: Request, response: Response, next: NextFunction) => {
-try {
-  
-  const filePath =  `C:/Development/codebase/cg-backend/src/controllers/forntend/uploads/${request.file.filename}`;
-  //__dirname + '/uploads/' + request.file.filename;
-  // console.log("filePath>>>>>",filePath)
-  console.log(`C:/Development/codebase/cg-backend/src/controllers/forntend/uploads/${request.file.filename}`)
-  readXlsxFile(filePath).then((data) => {
-    for (i in data) {
-      for (j in data[i]) {
-        console.log(data[i][j])
-      }
-    }
-  })
+  try {
+    let path = request.file.path;
+    const { user_id } = request.body;
 
-} catch (error) {
-  next(error)
-}
+    var workbook = XLSX.readFile(path);
+    var sheet_name_list = workbook.SheetNames;
+    let jsonData = XLSX.utils.sheet_to_json(
+      workbook.Sheets[sheet_name_list[0]]
+    );
+    if (jsonData.length === 0) {
+      return response.status(400).json({
+        success: false,
+        message: "xml sheet has no data",
+      });
+    }
+    // let savedData = await Lead.create(jsonData);
+    const newData = []
+    const newMap = jsonData.map((singleData)=>{
+      newData.push(new QuestionModel({ 
+        "options": [singleData.option1, singleData.option2 , singleData.option3, singleData.option4],
+        "question": singleData.question,
+        "rightoption": singleData.rightoption,
+        "point": singleData.point,
+        "level": singleData.level,
+        "questiontype": singleData.questiontype,
+        "user_id": user_id,
+        "rightAnswerDescription": singleData.rightAnswerDescription
+      }))
+    })
+
+    QuestionModel.insertMany(newData).then((docs) => {
+      return response.status(201).json({
+        success: true,
+        data: docs,
+        message: " rows added to the database",
+      });
+    }).catch((err) => {
+      return response.status(500).json({ success: false, message: err.message });
+    })
+    
+  } catch (err) {
+    return response.status(500).json({ success: false, message: err.message });
+  }
 });
